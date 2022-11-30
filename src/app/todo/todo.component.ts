79054b38c6e717be172b9b90c007bc0f66807ee1
Salooma <saloomaali2000@gmail.com>
2022-11-30 08:23:22 +0530

    to do

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent {
+
+  constructor(private api:ApiService){
+
+    api.fetchToDo().subscribe(
+      (response)=>{
+
+        this.todo = response;
+      }
+    )
+  }
+
+  todo:any = []
+}
